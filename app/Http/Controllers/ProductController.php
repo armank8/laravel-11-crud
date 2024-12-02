82ca9e80fa90c91f8d3d74e997e65d602323c4ce
Arman-Kabir <armankabir8@gmail.com>
2024-12-02 14:30:03 +0600

    create prtoduct page half done with controller and routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    //This method will show products page
+    public function index(){
+        
+    }
+    //This method will show create product page
+    public function create(){
+        return view('products.create');
+    }
+    //This method will store a product in db
+    public function store(){
+
+    }
+    //This method will show edit product page
+    public function edit(){
+
+    }
+    //This method will update a product
+    public function update(){
+
+    }
+    //This method will delete a  product
+    public function destroy(){
+
+    }
 }
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple laravel 11 Crud</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="bg-dark py-3 bg-blue-600 text-center">
+        <h3 class="text-white">Simple laravel 11 Crud</h3>
+    </div>
+    <!-- container -->
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10">
+                <div class="card border-0 shadow-lg my-4">
+                    <div class="card-header">
+                        <h3>Create Product</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="" class="form-label"></label>
+                            <input type="text" class="form-control-lg" placeholder="Name" name="name">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
