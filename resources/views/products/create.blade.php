260585598ee504e87c487ec2a7187eb475d4ea82
Arman-Kabir <armankabir8@gmail.com>
2024-12-04 12:33:27 +0600

    create product form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -18,13 +18,37 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-10">
                 <div class="card border-0 shadow-lg my-4">
-                    <div class="card-header">
-                        <h3>Create Product</h3>
+                    <div class="card-header bg-dark">
+                        <h3 class="text-white">Create Product</h3>
                     </div>
                     <div class="card-body">
                         <div class="mb-3">
-                            <label for="" class="form-label"></label>
-                            <input type="text" class="form-control-lg" placeholder="Name" name="name">
+                            <label for="name" class="form-label h5">Name</label>
+                            <input type="text" class="form-control form-control-lg" placeholder="Name" name="name">
+                        </div>
+                        <!-- SKU -->
+                        <div class="mb-3">
+                            <label for="Sku" class="form-label h5">Sku</label>
+                            <input type="text" class="form-control form-control-lg" placeholder="sku" name="Sku">
+                        </div>
+                        <!-- Price -->
+                        <div class="mb-3">
+                            <label for="Price" class="form-label h5">Price</label>
+                            <input type="text" class="form-control form-control-lg" placeholder="Price" name="Price">
+                        </div>
+                        <!--Description  -->
+                        <div class="mb-3">
+                            <label for="Price" class="form-label h5">Description</label>
+                            <textarea class="form-control form-control-lg" placeholder="Description" name="Description"
+                                rows="10" cols="30"></textarea>
+                        </div>
+                        <!-- Image -->
+                        <div class="mb-3">
+                            <label for="Image" class="form-label h5">Image</label>
+                            <input type="file" class="form-control form-control-lg" placeholder="Image" name="Image">
+                        </div>
+                        <div class="d-grid">
+                            <button class="btn btn-lg btn-primary">Submit</button>
                         </div>
                     </div>
                 </div>
